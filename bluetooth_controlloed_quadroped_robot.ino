5aa0279b7c8a5c79d28f60f4ab0f12055e36ee6e
siddharthaconfusedlad <98210719+siddharthaconfusedlad@users.noreply.github.com>
2022-01-22 18:09:25 +0530

    Update bluetooth_controlloed_quadroped_robot.ino

--- bluetooth_controlloed_quadroped_robot.ino
+++ bluetooth_controlloed_quadroped_robot.ino
@@ -1,10 +1,71 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVOMIN  150 
+#define SERVOMAX  600 
+#define USMIN  600
+#define USMAX  2400 
+#define SERVO_FREQ 50 
+
+
+uint8_t servonum = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("15 channel Servo test!");
 
+  pwm.begin();
+ 
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ);  
+
+  delay(10);
+}
+
+
+void setServoPulse(uint8_t n, double pulse) {
+  double pulselength;
+  
+  pulselength = 1000000;   
+  pulselength /= SERVO_FREQ;  
+  Serial.print(pulselength); Serial.println(" us per period"); 
+  pulselength /= 4096;  // 
+  Serial.print(pulselength); Serial.println(" us per bit"); 
+  pulse *= 1000000;  // convert input seconds to us
+  pulse /= pulselength;
+  Serial.println(pulse);
+  pwm.setPWM(n, 0, pulse);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
+  Serial.println(servonum);
+  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+    pwm.setPWM(servonum, 0, pulselen);
+  }
 
+  delay(500);
+  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+    pwm.setPWM(servonum, 0, pulselen);
+  }
+
+  delay(500);
+
+ 
+  for (uint16_t microsec = USMIN; microsec < USMAX; microsec++) {
+    pwm.writeMicroseconds(servonum, microsec);
+  }
+
+  delay(500);
+  for (uint16_t microsec = USMAX; microsec > USMIN; microsec--) {
+    pwm.writeMicroseconds(servonum, microsec);
+  }
+
+  delay(500);
+
+  servonum++;
+  if (servonum > 15) servonum = 0; 
 }
