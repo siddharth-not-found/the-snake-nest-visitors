51156217d8db6daf00274de0dddc01d9b36bbd55
siddharthaconfusedlad <98210719+siddharthaconfusedlad@users.noreply.github.com>
2022-01-23 09:48:33 +0530

    Update bluetooth_controlloed_quadroped_robot.ino

--- bluetooth_controlloed_quadroped_robot.ino
+++ bluetooth_controlloed_quadroped_robot.ino
@@ -4,15 +4,30 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
+
 #define SERVOMIN  150 
 #define SERVOMAX  600 
-#define USMIN  600
+#define USMIN  600 
 #define USMAX  2400 
 #define SERVO_FREQ 50 
 
-
-uint8_t servonum = 0;
-
+// our servo 
+uint8_t FRS = 0;
+uint8_t servonum1 = 1;
+uint8_t servonum2 = 2;
+uint8_t FRK = 3;
+uint8_t BRS = 4;
+uint8_t servonum5 =5;
+uint8_t servonum6 = 6;
+uint8_t BRK = 7;
+uint8_t BLA = 8;
+uint8_t servonum9 = 9;
+uint8_t servonum10 = 10;
+uint8_t BLK = 11;
+uint8_t FLS = 12;
+uint8_t servonum = 13;
+uint8_t servonum14 = 14;
+uint8_t FLK = 15;
 void setup() {
   Serial.begin(9600);
   Serial.println("15 channel Servo test!");
@@ -21,7 +36,6 @@ void setup() {
  
   pwm.setOscillatorFrequency(27000000);
   pwm.setPWMFreq(SERVO_FREQ);  
-
   delay(10);
 }
 
@@ -29,10 +43,10 @@ void setup() {
 void setServoPulse(uint8_t n, double pulse) {
   double pulselength;
   
-  pulselength = 1000000;   
-  pulselength /= SERVO_FREQ;  
+  pulselength = 1000000;  
+  pulselength /= SERVO_FREQ;   
   Serial.print(pulselength); Serial.println(" us per period"); 
-  pulselength /= 4096;  // 
+  pulselength /= 4096;  // 12 bits of resolution
   Serial.print(pulselength); Serial.println(" us per bit"); 
   pulse *= 1000000;  // convert input seconds to us
   pulse /= pulselength;
@@ -41,31 +55,48 @@ void setServoPulse(uint8_t n, double pulse) {
 }
 
 void loop() {
+  
  
-  Serial.println(servonum);
-  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-    pwm.setPWM(servonum, 0, pulselen);
+  
+  Serial.println(FLS);
+delay(500);
+  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+    pwm.setPWM(FLS, 0, pulselen);
   }
-
+  Serial.println(FLK);
   delay(500);
   for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-    pwm.setPWM(servonum, 0, pulselen);
+    pwm.setPWM(FLK, 0, pulselen);
   }
-
-  delay(500);
-
- 
-  for (uint16_t microsec = USMIN; microsec < USMAX; microsec++) {
-    pwm.writeMicroseconds(servonum, microsec);
+  Serial.println(BLA);
+   delay(500);
+  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+    pwm.setPWM(BLA, 0, pulselen);
   }
-
-  delay(500);
-  for (uint16_t microsec = USMAX; microsec > USMIN; microsec--) {
-    pwm.writeMicroseconds(servonum, microsec);
+    Serial.println(BLK);
+   delay(500);
+  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+    pwm.setPWM(BLK, 0, pulselen);
+    delay (1000);
   }
-
+  Serial.println(FRS);
+   delay(500);
+  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+    pwm.setPWM(FRS, 0, pulselen);
+  }
+Serial.println(FRK);
   delay(500);
-
-  servonum++;
-  if (servonum > 15) servonum = 0; 
+  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+    pwm.setPWM(FRK, 5, pulselen);
+  }
+  Serial.println(BRS);
+  delay(500);
+  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+    pwm.setPWM(BRS, 0, pulselen);
+  }
+    Serial.println(BRK);
+   delay(500);
+  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+    pwm.setPWM(BRK, 0, pulselen);
+  }
 }
